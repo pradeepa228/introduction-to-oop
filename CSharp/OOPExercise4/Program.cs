7cf03efa5ba9d7673f9fc1db72b9556b65673903
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-09-04 22:43:43 +0100

    Create Program.cs

---
+++ CSharp/OOPExercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using OOPExercise4;
+
+namespace OOPExercise4
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Pigeon pippa = new Pigeon("Pippa", "Grey");
+            pippa.Speak();
+            pippa.Fly();
+            pippa.EatPizza();
+
+            Penguin pingu = new Penguin("Pingu", "Black & White");
+            pingu.Speak();
+            pingu.Fly();
+
+            Parrot Keeki = new Parrot("Keeki", "Green");
+            Keeki.Speak();
+
+        }
+    }
+
+}
+
+

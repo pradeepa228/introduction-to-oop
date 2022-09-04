e464fc831dd47dc45258cd8b10451b248d3edb43
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-09-04 22:47:42 +0100

    Commit Exercise4 in OOP
    Commit Exercise4 in OOP

---
+++ CSharp/OOPExercise4/Bird.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPExercise4
+{
+    public abstract class Bird
+    {
+        public string Name { get; private set; }
+        public string Color { get; private set; }
+
+        public Bird(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public abstract void Speak();
+
+        public virtual void Fly()
+        {
+            Console.WriteLine($"Hi there! I can fly.");
+        }
+    }
+    public class Pigeon : Bird
+    {
+        public Pigeon(string name, string color) : base(name, color)
+        {
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine($"Yo! I'm {Name} and I'm a {Color} pigeon.");
+        }
+
+
+        public void EatPizza()
+        {
+            Console.WriteLine("Delicious pizza!");
+        }
+
+    }
+
+    public class Penguin : Bird
+    {
+        public Penguin(string name, string color) : base(name, color)
+        {
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine($"Hi! I'm {Name} and I'm a {Color} penguin.");
+        }
+
+        public override void Fly()
+        {
+            Console.WriteLine($"Whoops, I can't fly. Waddle waddle.");
+        }
+    }
+
+    public class Parrot : Bird
+    {
+        public Parrot(string name, string color) : base(name, color)
+        {
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine($"Hi! Am Parrot. I can dance");
+        }
+
+    }
+}

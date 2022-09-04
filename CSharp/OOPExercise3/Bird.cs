c070b52f6c093c63348aef8bbb00e85c855b2ea4
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-09-04 22:49:24 +0100

    Commit Exercise2&3 in OOP
    Commit Exercise2&3 in OOP

---
+++ CSharp/OOPExercise3/Bird.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPExercise3
+{
+    public class Bird
+    {
+        public string Name { get; private set; }
+        public string Color { get; private set; }
+
+        public Bird(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine($"My name is {Name} and I am a {Color} bird.");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"I'm {Name} and I can fly high in the blue sky!");
+        }
+    }
+    public class Pigeon : Bird
+    {
+       public Pigeon(string name, string color) : base(name, color)
+        {
+        }
+
+        public void EatPizza()
+        {
+            Console.WriteLine("Delicious pizza!");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("I'm a sleeping pigeon");
+        }
+    }
+}
---
+++ CSharp/OOPExercise3/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPExercise3;
+
+namespace OOPExercise3
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Bird myBird = new Bird("Tweety", "Yellow");
+            myBird.Speak();
+            myBird.Fly();
+
+            Pigeon pippa = new Pigeon("Pippa", "Grey");
+            pippa.Speak();
+            pippa.Fly();
+            pippa.EatPizza();
+            pippa.Sleep();
+
+        }
+    }
+
+}
+
+
